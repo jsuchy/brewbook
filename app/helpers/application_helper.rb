daf318a15d1c971cdb6b6ec6de6461ec14e4b17f
Eric Koslow <ekoslow@gmail.com>
2012-11-11 11:51:55 -0600

    Refactored Calculate Views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,45 @@
 module ApplicationHelper
+  def control_group(options)
+    id = options.fetch(:id)
+    content_tag(:div, :id => id, :class => "control-group") do
+      _inner_control_group(options) 
+    end
+  end
+
+  private
+
+  def _inner_control_group(options)
+    label = options.fetch(:label)
+
+    label_tag(nil, label, :class => "control-label") +
+    _control_group_controls(options)
+  end
+
+  def _control_group_controls(options)
+    name = options.fetch(:name)
+    input = _mini_input(name)
+    addon = options[:addon]
+
+    content_tag(:div, :class => "controls") do
+      if addon
+        _with_add_on(addon) do
+          input
+        end
+      else
+        input
+      end +
+      content_tag(:span, nil, :class => "help-inline")
+    end
+  end
+
+  def _with_add_on(addon, &block)
+    content_tag(:div, :class => "input-append") do
+      yield +
+      content_tag(:span, addon.html_safe, :class => "add-on")
+    end
+  end
+
+  def _mini_input(name)
+    text_field_tag(name, nil, :class => "input-mini")
+  end
 end
