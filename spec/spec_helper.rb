c424f27f379bc2ec977c4963df874349ad1274f0
Eric Koslow <ekoslow@gmail.com>
2012-11-12 10:02:31 -0600

    Manully require mocks/base first so Travis is happy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ $: << APP_ROOT+"/lib"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
+require 'support/mocks/base'
 Dir[APP_ROOT+"/spec/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
