22b587c07cb961d5c76524edf45bb33c3fb46471
Eric Koslow <ekoslow@gmail.com>
2012-11-13 11:23:17 -0600

    Cleaned up spec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,26 +19,11 @@ require 'support/mocks/base'
 Dir[APP_ROOT+"/spec/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
+  config.before(:each) { DataMapper.auto_migrate! }
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.before(:each) { DataMapper.auto_migrate! }
   config.order = "random"
 end
