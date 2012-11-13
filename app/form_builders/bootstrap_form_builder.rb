ac956a6b7a44d96cb864badc598bebf4f70fa50a
Eric Koslow <ekoslow@gmail.com>
2012-11-13 11:20:58 -0600

    Added custom Form Builder

---
+++ app/form_builders/bootstrap_form_builder.rb
@@ -0,0 +1,48 @@
+class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+  delegate :content_tag, :tag, to: :@template
+
+  def text_field(name, *args)
+    _field_label(name, *args).to_s.html_safe + super
+  end
+
+  def text_area(name, *args)
+    _field_label(name, *args).to_s.html_safe + super
+  end
+
+  def check_box(name, *args)
+    content_tag(:label, name.to_s.humanize, :class => "checkbox") do
+      super
+    end
+  end
+
+  def number_field(method, options = {})
+    options[:step] = "0.1"
+    options[:min] = "0"
+    options[:class] ||= "input-small"
+
+    input = super(method, options)
+
+    _field_label(method, options).to_s.html_safe +
+    if options[:addon]
+      content_tag(:div, :class => "input-append") do
+        input +
+        content_tag(:span, options[:addon].html_safe, :class => "add-on")
+      end
+    else
+      input
+    end
+  end
+
+  def button(text=nil)
+    msg = text || object.new? ? "Save" : "Update"
+    content_tag(:button, msg, :class => "btn", :type => "submit")
+  end
+
+  private
+
+  def _field_label(name, *args)
+    options = args.extract_options!
+    label_off = options.delete(:label_off)
+    label(name, options[:label]) unless label_off
+  end
+end
---
+++ spec/helpers/bootstrap_form_builder_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+require 'rails_helper'
+
+shared_examples "should generate a label and an input" do
+  it "should generate a label and an input" do
+    expect(generated.css("label").first).to_not be_nil
+    expect(generated.css("input").first).to_not be_nil
+  end
+end
+
+describe "BootstrapFormBuilder" do
+  subject(:builder) do
+    mock = stub(:name => "Eric", :isTrue => true, :number => 5, :new? => true)
+    BootstrapFormBuilder.new(:recipe, mock, helper, {}, nil) 
+  end
+
+  describe "#text_field" do
+    let(:generated) { Nokogiri::HTML(builder.text_field(:name)) }
+    
+    include_examples "should generate a label and an input"
+  end
+
+  describe "#text_area" do
+    let(:generated) { Nokogiri::HTML(builder.text_area(:name)) }
+    
+    it "should generate a label and an textarea" do
+      expect(generated.css("label").first).to_not be_nil
+      expect(generated.css("textarea").first).to_not be_nil
+    end
+  end
+
+  describe "#check_box" do
+    before do
+      builder.object.stub(:isTrue => true)
+    end
+    let(:generated) { Nokogiri::HTML(builder.check_box(:isTrue)) }
+    
+    include_examples "should generate a label and an input"
+
+    it "should wrap the input with a label" do
+      expect(generated.css("label input").first).to_not be_nil
+    end
+  end
+
+  describe "#number_field" do
+    let(:generated) { Nokogiri::HTML(builder.number_field(:number)) }
+    
+    include_examples "should generate a label and an input"
+
+    describe "the input" do
+      let(:input) { generated.css("input").first }
+
+      it "has a type of number" do
+        expect(input.attr('type')).to eq("number")
+      end
+
+      it "has a step of 0.1" do
+        expect(input.attr('step')).to eq("0.1")
+      end
+    end
+
+    context "with an add on" do
+      let(:generated) { Nokogiri::HTML(builder.number_field(:number, :addon => "a")) }
+
+      describe "the input-append div" do
+        let(:input_append) { generated.css(".input-append").first }
+
+        it "has a input and a span" do
+          expect(input_append.css("input").first).to_not be_nil
+          expect(input_append.css("span").first).to_not be_nil
+        end
+
+        it "the span contains the addon" do
+          expect(input_append.css("span").first.text).to eq("a")
+        end
+
+      end
+    end
+  end
+
+  describe "#button" do
+    let(:generated) { Nokogiri::HTML(builder.button) }
+
+    describe "the generated button" do
+      let(:button) { generated.css("button").first }
+
+      it "has the vaue of 'save'" do
+        expect(button.text).to eq("Save")
+      end
+    end
+  end
+
+end
+
