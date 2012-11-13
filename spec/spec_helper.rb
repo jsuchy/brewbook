fc0cf258ba8cfc939910bd59ebc6cc53112618ac
Eric Koslow <ekoslow@gmail.com>
2012-11-13 16:43:27 -0600

    Only run auto_migrate! once

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ require 'support/mocks/base'
 Dir[APP_ROOT+"/spec/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  config.before(:each) { DataMapper.auto_migrate! }
+  config.before(:suite) { DataMapper.auto_migrate! }
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
